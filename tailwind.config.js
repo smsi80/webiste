8d54618b52eed63d9abcef962535215c0d5284d7
smsi80 <sinamehrabi80@gmail.com>
2022-01-09 17:48:23 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      fontFamily: {
+        arabic: ["Noto Sans Arabic", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
